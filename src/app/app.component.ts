2aa6167a1bc54e36380a3d1d72a0dea672727d61
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-13 21:04:39 +0530

    using services to perform get and post requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
 import { Post } from "./post.model";
+import { PostService } from "./post.service";
 
 @Component({
   selector: "app-root",
@@ -14,52 +15,34 @@ export class AppComponent implements OnInit {
   firebaseUrl =
     "https://angular-course-70bd6-default-rtdb.asia-southeast1.firebasedatabase.app/";
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postService: PostService) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    this.isFetching = true;
+    this.postService.fetchPosts().subscribe((posts) => {
+      // console.log(posts);
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
   }
 
   onCreatePost(postData: Post) {
     // Send Http request
     console.log(postData);
-
-    // requests are sent only when you subscribe
-    this.http
-      .post<{ name: string }>(this.firebaseUrl + "posts.json", postData)
-      .subscribe((responseData) => {
-        // console.log(responseData);
-      });
+    this.postService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
     // Send Http request
-    this.fetchPosts();
+    this.isFetching = true;
+    this.postService.fetchPosts().subscribe((posts) => {
+      // console.log(posts);
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
   }
 
   onClearPosts() {
     // Send Http request
-  }
-
-  private fetchPosts() {
-    this.isFetching = true;
-    this.http
-      .get<{ [key: string]: Post }>(this.firebaseUrl + "posts.json")
-      .pipe(
-        map((responseData) => {
-          const postsArray: Post[] = [];
-          for (const key in responseData) {
-            if (responseData.hasOwnProperty(key)) {
-              postsArray.push({ ...responseData[key], id: key });
-            }
-          }
-          return postsArray;
-        })
-      )
-      .subscribe((posts) => {
-        // console.log(posts);
-        this.isFetching = false;
-        this.loadedPosts = posts;
-      });
   }
 }
---
+++ src/app/post.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
+import { Post } from "./post.model";
+
+@Injectable({
+  providedIn: "root",
+})
+export class PostService {
+  firebaseUrl =
+    "https://angular-course-70bd6-default-rtdb.asia-southeast1.firebasedatabase.app/";
+
+  constructor(private http: HttpClient) {}
+
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = { title: title, content: content };
+    this.http
+      .post<{ name: string }>(this.firebaseUrl + "posts.json", postData)
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
+
+  fetchPosts() {
+    return this.http
+      .get<{ [key: string]: Post }>(this.firebaseUrl + "posts.json")
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      );
+  }
+}
