65cf19ff467aa098748f73b7e2ba21d7bcc32bf4
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-13 13:22:23 +0530

    sending a post request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
@@ -16,6 +16,16 @@ export class AppComponent implements OnInit {
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
+
+    //https://console.firebase.google.com/project/angular-course-70bd6/database/angular-course-70bd6-default-rtdb/data/~2F
+    this.http
+      .post(
+        "https://angular-course-70bd6-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json",
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
   }
 
   onFetchPosts() {
